c41687d34eda397f2be718fbb263a42c462cafb7
Nathan Stoltenberg <nds4@pdx.edu>
2022-02-11 22:57:58 -0800

    Added basic return

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 80;
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 
-app.get("/api/personal", (req, res) => {});
+async function makeAPIcall(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+app.get("/api/personal", (req, res) => {
+  const data = makeAPIcall("https://www.boredapi.com/api/activity");
+  data.then((x) => {
+    console.log(x);
+    res.send(x);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
