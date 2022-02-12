07256fd37c4aa759e7ec27e17f0fc108064b96d6
Nathan Stoltenberg <nds4@pdx.edu>
2022-02-11 22:38:14 -0800

    Added node-fetch and created a get route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 
+app.get("/api/personal", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
