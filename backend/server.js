5872cb26027d306eb486a9ef08655f20fd39eca3
dustin johnson <jdustin@pdx.edu>
2022-02-12 16:12:25 -0800

    Combined server.js and serverdb.js files to remove redundencies. Makes call to conn.js to determine if connection to database is established

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: ".server/config.env" });
 const PORT = process.env.PORT || 80;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 
+const dbo = require("./server/conn");
 app.listen(PORT, () => {
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
   console.log(`listening on port ${PORT}`);
 });
