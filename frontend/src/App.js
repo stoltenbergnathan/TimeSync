4930381143aa39a449147a86bcb55d608c019e63
Nathan Stoltenberg <nds4@pdx.edu>
2022-02-13 20:39:47 -0800

    Created base personal form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import "./Components/Personal";
+import Personal from "./Components/Personal";
+import PersonalForm from "./Components/PersonalForm";
 
 function App() {
   return (
-    <div>
-      <h1>Test App</h1>
-    </div>
+    // <div>
+    //   <h1>Test App</h1>
+    // </div>
+    <PersonalForm />
   );
 }
 
--- frontend/src/Components/PersonalForm.js
+++ frontend/src/Components/PersonalForm.js
@@ -1,9 +1,60 @@
-import React from "react";
+import { useState, React } from "react";
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
 
 function PersonalForm() {
+  const [data, setData] = useState({
+    type: "",
+    minprice: "",
+    maxprice: "",
+    minaccessibility: "",
+    maxaccessibility: "",
+  });
+
   return (
     <div>
-      <h1>Test PersonalForm</h1>
+      <form>
+        <label title="Type" htmlFor="type-select">
+          Type:
+        </label>
+        <br />
+        <select id="type-select">
+          <option value="charity">charity</option>
+          <option value="cooking">cooking</option>
+          <option value="music">music</option>
+          <option value="diy">diy</option>
+          <option value="education">education</option>
+          <option value="social">social</option>
+          <option value="busywork">busywork</option>
+          <option value="recreational">recreational</option>
+        </select>
+        <br />
+        <label title="Cost" htmlFor="cost-slider">
+          Cost:
+        </label>
+        <br />
+        <Nouislider
+          accessibility
+          range={{ min: 0, max: 100 }}
+          start={[20, 80]}
+          connect
+          pips={{ mode: "steps", stepped: true, density: 10 }}
+          step={10}
+        />
+        <br />
+        <label title="Cost" htmlFor="cost-slider">
+          Accessibility:
+        </label>
+        <br />
+        <Nouislider
+          accessibility
+          range={{ min: 0, max: 100 }}
+          start={[20, 80]}
+          connect
+          pips={{ mode: "steps", stepped: true, density: 10 }}
+          step={10}
+        />
+      </form>
     </div>
   );
 }
