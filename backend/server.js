7339da919de272779ad71d6816254428a092d606
Nathan Stoltenberg <nds4@pdx.edu>
2022-02-15 22:05:38 -0800

    Added frontend components and fixed DB errors

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: ".server/config.env" });
+require("dotenv").config();
 const PORT = process.env.PORT || 80;
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
-app.use(cors());
-app.use(express.json());
+const mongoose = require("mongoose");
+const connectionString = process.env.ATLAS_URI;
 
-app.get("/", (req, res) => {
-  res.send("HOME PAGE");
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+  })
+  .catch((error) => console.log(error));
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connected to DB successfully");
 });
 
 async function makeAPIcall(url) {
@@ -18,8 +24,10 @@ async function makeAPIcall(url) {
   return data;
 }
 
-// See link below for API data vars
-// https://github.com/drewthoennes/Bored-API/blob/a978ac490c4c3aff555e7453ad8e577b658f8864/src/backend/models/Activity.js
+app.get("/", (req, res) => {
+  res.send("HOME PAGE");
+});
+
 app.get("/api/personal", (req, res) => {
   let query = req.query;
   console.log(query);
@@ -35,13 +43,5 @@ app.get("/api/personal", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}`);
-=======
-const dbo = require("./server/conn");
-app.listen(PORT, () => {
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
   console.log(`listening on port ${PORT}`);
 });
