f75cfcc81c954f5fcb70a16aa28143f7e81f59bc
dustin johnson <jdustin@pdx.edu>
2022-02-25 21:07:47 -0800

    Major navbar changes, Homepage getting closer to final stage

--- frontend/src/Components/Homepage.js
+++ frontend/src/Components/Homepage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function Homepage() {
   return (
     <div>
-      <h1>Test Page</h1>
+      <div className=" d-flex justify-content-center">
+        <Button variant="primary  w-25 m-1" size="lg">
+          Area Feed
+        </Button>
+        <Button variant="success w-25 m-1" size="lg">
+          Personal Feed
+        </Button>
+      </div>
+      <div className="container d-flex justify-content-center w-75 border h-100">
+        <h1>Feed goes here</h1>
+      </div>
     </div>
   );
 }
