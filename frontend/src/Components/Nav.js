7d418c0a4df93410acf55ccbbc3d2ad3e57425f4
dustin johnson <jdustin@pdx.edu>
2022-02-27 18:00:03 -0800

    Nav.js not working

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,33 +1,31 @@
 import React from "react";
 import logo from "../assets/clock.gif";
-import { Container, NavDropdown, Navbar } from "react-bootstrap";
+import { NavDropdown, Navbar } from "react-bootstrap";
 
 function Nav() {
   return (
-    <div className="navbar p-0">
-      <Navbar className="navbar navbar-expand nav-fill  w-100 navbar-dark bg-dark">
-        <Container>
-          <Navbar.Brand href="/">
-            <img
-              src={logo}
-              style={{ "max-width": "6%", "margin-right": "5px" }}
-              alt="spinning clock"
-            />
-            TimeSync
-          </Navbar.Brand>
-          <Navbar.Collapse>
-            <Nav className="navbar-nav ml-auto">
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/events">Find Events</NavDropdown.Item>
-                <NavDropdown.Item href="/activities">
-                  Get Activities
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/messages">Messages</Nav.Link>
-              <Nav.Link href="/login">Profile</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">
+          <img
+            width="30"
+            height="30"
+            src={logo}
+            alt="spinning clock"
+            className="d-inline-block align-top"
+          />
+          TimeSync
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#events">Find Events</NavDropdown.Item>
+            <NavDropdown.Item href="#activities">
+              Get Activities
+            </NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#messages">Messages</Nav.Link>
+          <Nav.Link href="#login">Profile</Nav.Link>
+        </Nav>
       </Navbar>
     </div>
   );
