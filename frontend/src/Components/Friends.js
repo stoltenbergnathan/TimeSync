6121e6b26cd662befa8e3939f9444f91a42d6ed6
Nathan Stoltenberg <nds4@pdx.edu>
2022-03-03 23:47:38 -0800

    Added selection bar

---
+++ frontend/src/Components/Friends.js
@@ -0,0 +1,5 @@
+function Friends() {
+  return <h1>Friends</h1>;
+}
+
+export default Friends;
--- frontend/src/Components/ProfilePage.js
+++ frontend/src/Components/ProfilePage.js
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import { Button, Col, Container, Nav, Row } from "react-bootstrap";
+import Settings from "./Settings";
+import Friends from "./Friends";
+
 function ProfilePage() {
-  return <h1>ProfilePage</h1>;
+  const [user, setUser] = useState("username");
+  const [friendList, setFriendList] = useState([]);
+  const [selection, setSelection] = useState("");
+
+  const handleLogOut = (e) => {
+    console.log("LOGOUT");
+  };
+
+  const handleSwitch = (e) => {
+    console.log(e.target.innerHTML);
+    setSelection(e.target.innerHTML);
+  };
+
+  const renderSelection = () => {
+    console.log(selection);
+    switch (selection) {
+      case "Settings":
+        return <Settings />;
+      case "Friends":
+        return <Friends />;
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <Container>
+      <header>
+        <h1>{user}</h1>
+        <Button onClick={handleLogOut}>Log Out</Button>
+      </header>
+      <Row>
+        <Col className="col-3">
+          <Nav variant="tabs" className="flex-column">
+            <Nav.Link onClick={handleSwitch}>Settings</Nav.Link>
+            <Nav.Link onClick={handleSwitch}>Friends</Nav.Link>
+          </Nav>
+        </Col>
+        <Col className="col-3">{renderSelection()}</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default ProfilePage;
