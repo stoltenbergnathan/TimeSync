f42b51189978662a87c02dcc03cd90683ab6c4ce
Nathan Stoltenberg <nds4@pdx.edu>
2022-03-03 23:56:24 -0800

    Added password change form

--- frontend/src/Components/Settings.js
+++ frontend/src/Components/Settings.js
@@ -1,5 +1,57 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
 function Settings() {
-  return <h1>Settings</h1>;
+  const [passwordChangeData, setChangeData] = useState({
+    currentPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+    console.log(passwordChangeData);
+  };
+
+  return (
+    <Form onSubmit={handlePasswordChange}>
+      <Form.Label>Change Password:</Form.Label>
+      <Form.Text>Current Password</Form.Text>
+      <Form.Control
+        type="password"
+        value={passwordChangeData.currentPassword}
+        onChange={(e) =>
+          setChangeData({
+            ...passwordChangeData,
+            currentPassword: e.target.value,
+          })
+        }
+      />
+      <Form.Text>New Password</Form.Text>
+      <Form.Control
+        type="password"
+        value={passwordChangeData.newPassword}
+        onChange={(e) =>
+          setChangeData({
+            ...passwordChangeData,
+            newPassword: e.target.value,
+          })
+        }
+      />
+      <Form.Text>Confirm new Password</Form.Text>
+      <Form.Control
+        type="password"
+        value={passwordChangeData.confirmNewPassword}
+        onChange={(e) =>
+          setChangeData({
+            ...passwordChangeData,
+            confirmNewPassword: e.target.value,
+          })
+        }
+      />
+      <Button type="submit">Change Password</Button>
+    </Form>
+  );
 }
 
 export default Settings;
